9c6bfe20f3f8ce7d993aa68a6d69735783949c16
liujunhong2 <2499185166@qq.com>
2025-06-13 15:09:43 +0800

    modify name from CH to EN

--- db.sql
+++ db.sql
@@ -1,3 +1,4 @@
+-- Part1
 -- IF DB_ID(N'TGP') IS NOT NULL
 --     DROP DATABASE TGP1;
 -- GO
@@ -90,3 +91,91 @@
 --     compare_flag CHAR(1) NOT NULL CHECK(compare_flag IN('>','=','<')),
 --     created_at DATETIME2 NOT NULL DEFAULT SYSUTCDATETIME()
 -- );
+
+-- Part2
+-- USE TGP;
+-- GO
+
+-- -- 1.1 省级年度数据 Staging
+-- IF OBJECT_ID('dbo.StagingProvince', 'U') IS NOT NULL
+--     DROP TABLE dbo.StagingProvince;
+-- GO
+-- CREATE TABLE dbo.StagingProvince(
+--     年度标识        INT              NULL,
+--     省份编码        NVARCHAR(50)     NULL,
+--     省份名称        NVARCHAR(100)    NULL,
+--     地区生产总值     DECIMAL(18,2)    NULL,
+--     人均地区生产总值  DECIMAL(18,2)    NULL,
+--     年底人口数       BIGINT           NULL,
+--     地区进出口总额    DECIMAL(18,2)    NULL,
+--     地区出口总额      DECIMAL(18,2)    NULL,
+--     地区进口总额      DECIMAL(18,2)    NULL,
+--     外资企业年底注册数量 DECIMAL(18,2) NULL,
+--     外资企业投资总额    DECIMAL(18,2) NULL,
+--     外资企业注册资本    DECIMAL(18,2) NULL
+-- );
+-- GO
+
+-- -- 1.2 地市级年度数据 Staging
+-- IF OBJECT_ID('dbo.StagingCity', 'U') IS NOT NULL
+--     DROP TABLE dbo.StagingCity;
+-- GO
+-- CREATE TABLE dbo.StagingCity(
+--     年度标识       INT           NULL,
+--     城市名称       NVARCHAR(100) NULL,
+--     城市代码       NVARCHAR(50)  NULL,
+--     省份名称       NVARCHAR(100) NULL,
+--     生产总值       DECIMAL(18,2) NULL,
+--     年末总人口       BIGINT        NULL,
+--     人口密度       DECIMAL(18,2) NULL,
+--     外商新签合同数   DECIMAL(18,2) NULL,
+--     外商协议投资金额 DECIMAL(18,2) NULL,
+--     外商实际投资额   DECIMAL(18,2) NULL
+-- );
+-- GO
+
+-- -- 1.3 县级年度数据 Staging
+-- IF OBJECT_ID('dbo.StagingCounty', 'U') IS NOT NULL
+--     DROP TABLE dbo.StagingCounty;
+-- GO
+-- CREATE TABLE dbo.StagingCounty(
+--     年度标识    INT           NULL,
+--     AreaID     NVARCHAR(50)  NULL,  -- 如果需要可存储，但插入目标表无此列
+--     县域名称    NVARCHAR(100) NULL,
+--     隶属城市    NVARCHAR(100) NULL,
+--     隶属省份    NVARCHAR(100) NULL,
+--     土地面积    DECIMAL(18,2) NULL,
+--     年末总人口   BIGINT        NULL,
+--     地区生产总值  DECIMAL(18,2) NULL
+-- );
+-- GO
+
+USE TGP;
+GO
+BULK INSERT dbo.StagingProvince
+FROM '/var/opt/mssql/data/省级数据-年度.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    TABLOCK
+);
+GO
+BULK INSERT dbo.StagingCity
+FROM '/var/opt/mssql/data/市级数据-年度.csv'
+WITH (
+    FIRSTROW = 2,
+    FORMAT='CSV',
+    FIELDQUOTE='"',
+    TABLOCK
+);
+GO
+BULK INSERT dbo.StagingCounty
+FROM '/var/opt/mssql/data/县级数据-年度.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    TABLOCK
+);
+GO
